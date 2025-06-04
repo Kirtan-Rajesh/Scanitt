f36af3973e60a036a81a7f02420e6ffb68ffb8a9
Kirtan Rajesh <96264432+Kirtan-Rajesh@users.noreply.github.com>
2025-06-05 00:06:58 +0530

    Update models.py

--- models.py
+++ models.py
@@ -10,9 +10,11 @@ class User(db.Model, UserMixin):  # Add UserMixin here
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.Text)
+    password_hash = db.Column(db.Text, nullable=False) # Changed from db.String(128)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     documents = db.relationship('Document', backref='owner', lazy=True)
+
+   
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
